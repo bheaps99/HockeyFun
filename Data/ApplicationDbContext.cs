eacb094e2e5b41b82894e1308b7e6a95914fa396
Ben Heaps <bheaps99@gmail.com>
2020-10-20 20:41:42 -0700

    1

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HockeyFun.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HockeyFun.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Seed();
+        }
+
+        public DbSet<Team> Teams{ get; set; }
+        public DbSet<Player> Players { get; set; }
+
+
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HockeyFun.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        [ForeignKey("Team")]
+        public string TeamName { get; set; }
+        public Team Team { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public static List<Player> GetPlayers()
+        {
+            List<Player> players = new List<Player>() {
+                new Player {
+                    PlayerId = 1,
+                    FirstName = "Sven",
+                    LastName = "Baertschi",
+                    TeamName = "Canucks",
+                    Position = "Forward"
+                },
+                new Player {
+                    PlayerId = 2,
+                    FirstName = "Hendrik",
+                    LastName = "Sedin",
+                    TeamName = "Canucks",
+                    Position = "Left Wing"
+                },
+                new Player {
+                    PlayerId = 3,
+                    FirstName = "John",
+                    LastName = "Rooster",
+                    TeamName = "Flames",
+                    Position = "Right Wing"
+                },
+                new Player {
+                    PlayerId = 4,
+                    FirstName = "Bob",
+                    LastName = "Plumber",
+                    TeamName = "Oilers",
+                    Position = "Defense"
+                },
+            };
+
+            return players;
+        }
+    }
+}
